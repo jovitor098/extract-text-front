e1be06b8e079c9d499b94883cfce44d2e0f140e7
jovitor <joaovitorguilherme788@gmail.com>
2025-01-27 20:19:39 -0300

    change default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Extract text",
-  description: "Extract texts from images",
+    title: "Extract text",
+    description: "Extract texts from images",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+          <body>
+              <main>{children}</main>
+          </body>
+      </html>
   );
 }
