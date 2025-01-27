37eb45dc5195567af6c20650d0cca75760903ac5
jovitor <joaovitorguilherme788@gmail.com>
2025-01-27 20:28:16 -0300

    refactor: remove the handler submit

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import getTextFromImage from "@/services/uploadImageService"
-import { FormEvent } from "react"
+import { FormEvent } from "react";
 
-export default function Form(){
-    const handlerSubmit = async (ev: FormEvent<HTMLFormElement>) => {
-        ev.preventDefault();
-        const text = await getTextFromImage(ev.currentTarget);
-        console.log(text);
-    }
+interface props {
+    handlerSubmit: (ev: FormEvent<HTMLFormElement>) => void
+}
+
+export default function Form({ handlerSubmit }: props){
  return(
     <form onSubmit={handlerSubmit}>
         <label htmlFor="upload_image">Upload your image</label>
