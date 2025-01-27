5021f333a2fb4f1987f1ce87245d3b86bef6aec5
jovitor <joaovitorguilherme788@gmail.com>
2025-01-27 20:16:04 -0300

    fix: cros origin and type response

--- src/services/uploadImageService.ts
+++ src/services/uploadImageService.ts
@@ -1,10 +1,21 @@
-const getTextFromImage = async (form: HTMLFormElement) => {
-    const formData = new FormData(form);
-    const response = await fetch(process.env.API_URL!, {
-         body: formData
-    })
+interface ResponseText {
+    text: string
+}
 
-    return response.text
+const getTextFromImage = async (form: HTMLFormElement) => {
+    if (!process.env.NEXT_PUBLIC_API_URL)
+        throw new Error("Url to api not set");
+    
+    const formData = new FormData(form);
+    
+    const response = await fetch("/api/extract", {
+        body: formData,
+        method: "post",
+    });
+
+    const json: ResponseText = await response.json();
+
+    return json.text;
 };
 
 export default getTextFromImage;
