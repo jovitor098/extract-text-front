9ddf067afd30c6605ee306648a25b45345da3286
jovitor <joaovitorguilherme788@gmail.com>
2025-01-27 20:21:13 -0300

    feature: add function to handle submit and change to use client

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,6 +1,16 @@
+'use client';
+
+import getTextFromImage from "@/services/uploadImageService"
+import { FormEvent } from "react"
+
 export default function Form(){
+    const handlerSubmit = async (ev: FormEvent<HTMLFormElement>) => {
+        ev.preventDefault();
+        const text = await getTextFromImage(ev.currentTarget);
+        console.log(text);
+    }
  return(
-    <form>
+    <form onSubmit={handlerSubmit}>
         <label htmlFor="upload_image">Upload your image</label>
         <input type="file" name="upload_image" id="upload_image" />
         <button type="submit">Extract</button>
