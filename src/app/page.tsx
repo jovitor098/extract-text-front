7440bface3d96f6a68dddb9cf0bed6bf37210e3d
jovitor <joaovitorguilherme788@gmail.com>
2025-01-27 20:27:44 -0300

    refactor: change the handler submit to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Form from "@/components/form";
+import getTextFromImage from "@/services/uploadImageService";
+import { FormEvent } from "react";
 
 export default function Home() {
+  const handlerSubmit = async (ev: FormEvent<HTMLFormElement>) => {
+      ev.preventDefault();
+      const text = await getTextFromImage(ev.currentTarget);
+      console.log(text);
+  }
+
   return (
       <>
           <h1>Extract texts from image</h1>
-          <Form />
+          <Form handlerSubmit={handlerSubmit} />
       </>
   );
 }
