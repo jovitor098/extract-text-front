f2d6432e2013b124d1cb7f0bfa72662749cb4cac
jovitor <joaovitorguilherme788@gmail.com>
2025-01-28 10:02:59 -0300

    feature: add spinner for loading text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import TextResult from "@/components/textResult";
 import getTextFromImage from "@/services/uploadImageService";
 import { FormEvent, useState } from "react";
 import styles from "./page.module.scss";
-import { Container } from "reactstrap";
+import { Container, Spinner } from "reactstrap";
 
 export default function Home() {
     const [text, setText] = useState("");
+    const [loadingText, setLoadingText] = useState(false);
     const handlerSubmit = async (ev: FormEvent<HTMLFormElement>) => {
         ev.preventDefault();
+        setText("");
+        setLoadingText(true);
         const textImage = await getTextFromImage(ev.currentTarget);
+        setLoadingText(false);
         setText(textImage);
     };
 
@@ -20,7 +24,11 @@ export default function Home() {
             <h1 className={styles.title}>Extract texts from image</h1>
             <Container className={styles.container}>
                 <FormImage handlerSubmit={handlerSubmit} />
-                {text.length > 0 ? <TextResult text={text} /> : null}
+                {text.length > 0 ? (
+                    <TextResult text={text} />
+                ) : loadingText ? (
+                    <Spinner />
+                ) : null}
             </Container>
         </>
     );
