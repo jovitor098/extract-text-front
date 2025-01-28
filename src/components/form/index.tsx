94d8bc5839cf37b175f581d6b1967536308fea9f
jovitor <joaovitorguilherme788@gmail.com>
2025-01-28 09:53:20 -0300

    feature: add handler function to upload image

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "./styles.module.scss";
 
@@ -7,13 +7,18 @@ interface props {
 }
 
 export default function FormImage({ handlerSubmit }: props) {
+    const [haveFile, setHaveFile] = useState(false)
+    const handleFileChange = (ev: FormEvent<HTMLInputElement>) => {
+        if (ev.currentTarget.files)
+            setHaveFile(true)
+    }
     return (
         <Form onSubmit={handlerSubmit} className={styles.form}>
             <FormGroup>
                 <Label htmlFor="upload_image" className={styles.label}>Upload your image</Label>
-                <Input type="file" id="upload_image" name="upload_image" />
+                <Input type="file" id="upload_image" name="upload_image" onChange={handleFileChange}/>
             </FormGroup>
-            <Button outline className={styles.button} type="submit">Extract</Button>
+            <Button outline disabled={!haveFile} className={styles.button} type="submit">Extract</Button>
         </Form>
     );
 }
