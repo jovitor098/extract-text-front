22875b095087f7c533ea0c1dbea2f65f22b89a4e
jovitor <joaovitorguilherme788@gmail.com>
2025-01-28 09:54:10 -0300

    styles: change styles code

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -7,18 +7,31 @@ interface props {
 }
 
 export default function FormImage({ handlerSubmit }: props) {
-    const [haveFile, setHaveFile] = useState(false)
+    const [haveFile, setHaveFile] = useState(false);
     const handleFileChange = (ev: FormEvent<HTMLInputElement>) => {
-        if (ev.currentTarget.files)
-            setHaveFile(true)
-    }
+        if (ev.currentTarget.files) setHaveFile(true);
+    };
     return (
         <Form onSubmit={handlerSubmit} className={styles.form}>
             <FormGroup>
-                <Label htmlFor="upload_image" className={styles.label}>Upload your image</Label>
-                <Input type="file" id="upload_image" name="upload_image" onChange={handleFileChange}/>
+                <Label htmlFor="upload_image" className={styles.label}>
+                    Upload your image
+                </Label>
+                <Input
+                    type="file"
+                    id="upload_image"
+                    name="upload_image"
+                    onChange={handleFileChange}
+                />
             </FormGroup>
-            <Button outline disabled={!haveFile} className={styles.button} type="submit">Extract</Button>
+            <Button
+                outline
+                disabled={!haveFile}
+                className={styles.button}
+                type="submit"
+            >
+                Extract
+            </Button>
         </Form>
     );
 }
